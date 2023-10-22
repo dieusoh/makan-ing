73525a16eef519751e95d0acea340e7474aba510
kevlw <kevlw@amazon.com>
2023-10-22 13:33:14 +0800

    added routing

--- Test-bench/ALS-test.py
+++ Test-bench/ALS-test.py
@@ -2,18 +2,22 @@ import boto3
 
 als = boto3.client('location')
 
-response = als.search_place_index_for_text(
-    BiasPosition=[
-        1.312090, 103.768012
-        ],
-    FilterCategories=[
-        'Restaurant',
-    ],
-    IndexName='kevin-test-index',
-    Language='en',
-    MaxResults=5,
-    Text='Singapore'
+## function that is given the user's curreent coordinates and the restaurant's coordinates and is calculates the time taken to walk between them. 
 
+def food_distance (user_lat, user_long, restaurant_lat, restaurant_long):
+    response = als.calculate_route(
+        CalculatorName = 'GrabCalculator',
+        DeparturePosition=[user_long, user_lat],
+        DestinationPosition=[restaurant_long, restaurant_lat],
+        TravelMode='Walking'
     )
- 
-print (response)
+    print (response)
+    Distance = response['Summary']['Distance']
+    Duration = response['Summary']['DurationSeconds']
+    return [Duration, Distance]
+
+user_lat = 1.3137922364061911
+user_long = 103.76257464133185
+restaurant_lat = 1.3122856851787625
+restaurant_long = 103.76777670497655
+print (food_distance(user_lat, user_long, restaurant_lat, restaurant_long))
